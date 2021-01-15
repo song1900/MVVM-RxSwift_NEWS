0056211879c7d474c98ddfe9cf87633309d486f3
songwoojin <woojin1900@gmail.com>
2021-01-15 15:46:46 +0900

    Initial Commit

---
+++ NEWS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NEWS
+//
+//  Created by SungHo Han on 2021/01/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
