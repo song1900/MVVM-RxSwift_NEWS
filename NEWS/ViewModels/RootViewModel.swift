cc227491ee02bfa012566c0bd5eb43998e03199d
song1900 <woojin1900@gmail.com>
2021-01-18 22:03:55 +0900

    RootViewModel 생성

---
+++ NEWS/ViewModels/RootViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  RootViewModel.swift
+//  NEWS
+//
+//  Created by 송우진 on 2021/01/18.
+//
+
+import Foundation
+import RxSwift
+
+final class RootViewModel {
+    let title = "NEWS"
+    
+    private let articleService: ArticleServiceProtocol
+    
+    init(articleService: ArticleServiceProtocol) {
+        self.articleService = articleService
+    }
+    
+    func fetchArticles() -> Observable<[Article]> {
+        articleService.fetchNews()
+    }
+}
